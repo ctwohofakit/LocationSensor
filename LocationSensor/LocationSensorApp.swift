78642537a7b03d75ce268add3b296ccb9ae23370
kit sitou <ctwohofa@gmail.com>
2026-06-13 09:33:01 -0700

    Initial Commit

---
+++ LocationSensor/LocationSensorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationSensorApp.swift
+//  LocationSensor
+//
+//  Created by Kit Sitou on 6/13/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationSensorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
